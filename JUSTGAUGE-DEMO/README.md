56c52ac0a1a18526a5a0dedef0dc77b22bdde9c0
Technobly <technobly@gmail.com>
2020-12-21 11:31:08 -0600

    Renamed Spark to Particle, minor formatting updates

--- JUSTGAUGE-DEMO/README.md
+++ JUSTGAUGE-DEMO/README.md
@@ -1,12 +1,12 @@
-Remote Spark justGauge Demo
+Remote Particle justGauge Demo
 ===========================
 
-Remote Spark with a Gauge for Engine Temperature.
+Remote Particle with a Gauge for Engine Temperature.
 
 Start the engine, and watch the temperature rise!
 
 Stop the engine, and watch it cool off.
 
-![iPhone5s](http://i.imgur.com/AgFjCYY.png)
+![justgauge](gauge.gif)
 
 [justGauge](http://justgage.com)
--- RemoteParticle.cpp
+++ RemoteParticle.cpp
@@ -1,62 +1,25 @@
-#define ON 1
-#define OFF 0
+#include "Particle.h"
+
+// SerialLogHandler logHandler(LOG_LEVEL_ALL);
+
+#define ON  (1)
+#define OFF (0)
 
 int startState = 0;
 int trunkState = 0;
 int lockState = 0;
-int engineTemp = 0;
 int STARTPIN = D7;
 int TRUNKPIN = D6;
 int LOCKPIN = D5;
 int UNLOCKPIN = D4;
 
 uint32_t UPDATE_INTERVAL = 10; // in milliseconds
-uint32_t FIFTEEN_MINUTES = 60*15*1000; // in milliseconds
+uint32_t FIFTEEN_MINUTES = 60 * 15 * 1000; // in milliseconds
 
-uint32_t lastTime = 0; // in milliseconds
 uint32_t startTime = 0; // in milliseconds
 
-void setup() {
-    pinMode(STARTPIN,OUTPUT);
-    pinMode(TRUNKPIN,OUTPUT);
-    pinMode(LOCKPIN,OUTPUT);
-    pinMode(UNLOCKPIN,OUTPUT);
-
-    digitalWrite(STARTPIN,LOW);
-    digitalWrite(TRUNKPIN,LOW);
-    digitalWrite(LOCKPIN,LOW);
-    digitalWrite(UNLOCKPIN,LOW);
-
-    Spark.function("start", startFunction);
-    Spark.function("trunk", trunkFunction);
-    Spark.function("lock", lockFunction);
-    Spark.function("unlock", unlockFunction);
-    
-    Spark.variable("startstate", &startState, INT);
-    Spark.variable("trunkstate", &trunkState, INT);
-    Spark.variable("lockstate", &lockState, INT);
-    Spark.variable("enginetemp", &engineTemp, INT);
-}
-
-void loop() {
-    // Update every 10ms (default)
-    if(millis() - lastTime > UPDATE_INTERVAL) {
-        // Set a new last time
-        lastTime = millis();
-    
-        engineTemp += 1;    // increase our ficticious temp
-        engineTemp %= 2047; // if > 2047, set back to 0;
-
-        if(startState == ON) {
-            if(millis() - startTime > FIFTEEN_MINUTES) {
-                startState = OFF;
-            }
-        }        
-    }
-}
-
 int startFunction(String args) {
-    if(startState == OFF) {
+    if (startState == OFF) {
         startState = ON; // engine started
         startTime = millis();
         digitalWrite(STARTPIN, HIGH); // Double press the start button to start
@@ -78,7 +41,7 @@ int startFunction(String args) {
 }
 
 int trunkFunction(String args) {
-    if(trunkState == OFF) {
+    if (trunkState == OFF) {
         trunkState = ON; // trunk popped
         digitalWrite(TRUNKPIN, HIGH); // Single press the trunk pop button
         delay(500);
@@ -91,7 +54,7 @@ int trunkFunction(String args) {
 }
 
 int lockFunction(String args) {
-    if(lockState == OFF) {
+    if (lockState == OFF) {
         lockState = ON; // locks locked
         digitalWrite(LOCKPIN, HIGH); // Single press the lock button
         delay(500);
@@ -101,11 +64,40 @@ int lockFunction(String args) {
 }
 
 int unlockFunction(String args) {
-    if(lockState == ON) {
+    if (lockState == ON) {
         lockState = OFF; // locks unlocked
         digitalWrite(UNLOCKPIN, HIGH); // Single press the unlock button
         delay(500);
         digitalWrite(UNLOCKPIN, LOW);
     }
     return 200; // This is checked in the web app controller for validation
+}
+
+void setup() {
+    pinMode(STARTPIN, OUTPUT);
+    pinMode(TRUNKPIN, OUTPUT);
+    pinMode(LOCKPIN, OUTPUT);
+    pinMode(UNLOCKPIN, OUTPUT);
+
+    digitalWrite(STARTPIN, LOW);
+    digitalWrite(TRUNKPIN, LOW);
+    digitalWrite(LOCKPIN, LOW);
+    digitalWrite(UNLOCKPIN, LOW);
+
+    Particle.function("start", startFunction);
+    Particle.function("trunk", trunkFunction);
+    Particle.function("lock", lockFunction);
+    Particle.function("unlock", unlockFunction);
+
+    Particle.variable("startstate", &startState, INT);
+    Particle.variable("trunkstate", &trunkState, INT);
+    Particle.variable("lockstate", &lockState, INT);
+}
+
+void loop() {
+    if (startState == ON) {
+        if (millis() - startTime > FIFTEEN_MINUTES) {
+            startState = OFF;
+        }
+    }
 }
